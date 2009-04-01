e16635770faecd5f102bd30e2233a7e204da7adf
Jonathan O. Baker <bakerj@mitre.org>
2009-04-01 14:18:54 +0000

    added doxygen main page

---
+++ src/mainpage.h
@@ -0,0 +1,39 @@
+/** @mainpage 
+*
+* @section intro Introduction
+* The OVAL Interpreter is a freely available reference implementation that
+* demonstrates the evaluation of OVAL Definitions. Based on a set of 
+* Definitions the interpreter collects system information, evaluates it,
+* and generates a detailed OVAL Results file.
+* <br/><br/>
+* For more information please see:
+* <ul>
+*    <li><a href="http://oval.mitre.org">OVAL Website</a></li>
+*    <li><a href="http://oval.mitre.org/language/">OVAL Language Documentation</a></li>
+*    <li><a href="http://oval.mitre.org/language/interpreter.html">OVAL Interpreter Information</a></li>
+* </ul>
+* 
+* Also note the existence of the following directories:
+* <ul>
+*    <li>docs - Contains the terms or use, build instructions, release notes.</li>
+*    <li>src - Contains all the source code.</li>
+* </ul>
+* 
+* @section contributions Code Contributions
+* 
+*
+* @section todo TODO's
+* The items below are large areas of development that need to be done:
+* <ul>
+*    <li>Port to Solaris</li>
+*    <li>Port to Mac OS</li>
+*    <li>Windows probe development.</li>
+* </ul>
+*
+* @section contact Contact Information
+* Please use the SourceForge.net project for reporting issues, requesting 
+* features and asking for general usage help. If you need to contact the OVAL
+* team for any other reason please send email to 
+* <a href="mailto:oval@mitre.org">oval@mitre.org</a>.
+*
+*/
