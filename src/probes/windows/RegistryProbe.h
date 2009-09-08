ddcc1122b64936937e6f3492bb1f186bb11947a4
Dan Haynes <dhaynes@mitre.org>
2009-09-08 18:43:06 +0000

    Added support for registry behaviors.

--- src/probes/windows/RegistryProbe.h
+++ src/probes/windows/RegistryProbe.h
@@ -31,19 +31,18 @@
 #define REGISTRYPROBE_H
 
 #pragma warning(disable:4786)
-
+#include "RegistryFinder.h"
 #include "AbsProbe.h"
-#include "WindowsCommon.h"
-
 #include <windows.h>
 
 using namespace std;
 
 /**
-	This class is responsbile for collecting registry information for windows registry_objects.
+	This class is responsible for collecting registry information for windows registry_objects.
 */
 class RegistryProbe : public AbsProbe {
 public:
+	/** RegistryProbe destructor. */
 	~RegistryProbe();
 	
 	/** Run the registry probe. Return a vector of Items. */
@@ -54,74 +53,36 @@ public:
 	
 
 private:
+	/** RegistryProbe constructor. */
 	RegistryProbe();
-
-	static RegistryProbe* instance;
 
 	/** Return a new Item created for storing registry information. */
 	Item* CreateItem();
 
-	/** Return the set of hives to be collected. 
-	    If the operation is set to equals simply return the hive as an ItemEntity.
-	*/
-	ItemEntityVector* GetHives(ObjectEntity* hive);
-	/** Return the set of keys to be collected. 
-		If the operation is set to equals simply return the key as an ItemEntity.
-	*/
-	ItemEntityVector* GetKeys(ObjectEntity* key, ItemEntity* hive);
-	/** Return the set of names to be collected. 
-		If the operation is set to equals simply return the name as an ItemEntity.
-	*/
-	ItemEntityVector* GetNames(ObjectEntity* name, ItemEntity* hive, ItemEntity* key);
-	
-	/** Gather new data and put in a Item object.
-		Return NULL if the key is not found.
-	*/
-	Item* GetRegistryKey(ItemEntity*, ItemEntity*, ItemEntity*);
-	/** Return a handle to a predefined root key on the system.
-		An application must open a key before it can add data to the registry. To open a
-		key, an application must supply a handle to another key in the registry that is
-		already open. The system defines predefined keys that are always open.  Determine
-		if one of these predefined keys can be used, and if so, return it.
-	
-		NOTE: We have used _strnicmp() instead of the string compare() function as we need
-		to do a compare without regard to case.
-	*/
-	HKEY GetRootKey(string);
-	/** Return a vector of hives as DefinitionEntities that match the pattern 
-		Supported hives:
-		"HKEY_CLASSES_ROOT"
-		"HKEY_CURRENT_CONFIG"
-		"HKEY_CURRENT_USER"
-		"HKEY_LOCAL_MACHINE"
-		"HKEY_USERS"
-		May throw REGEXException if the pattern is invalid.
-	*/
-	void GetMatchingHives(string, ItemEntityVector*, bool isRegex = true);
-	/** Search the registry for keys under the designated hive and input key 
-		that match the pattern. Add all matches to the keys vector. For every sub key
-		found make recursive call. Stop when all subkeys have been searched.
-	*/
-	void GetMatchingKeys(string hiveIn, string keyIn, string pattern, ItemEntityVector* keys, bool isRegex = true);
-	/** Search the registry for names under the designated hive and input key 
-		that match the pattern. Add all matches to the names vector. 
-		Stop when all names have been searched
-	*/
-	void GetMatchingNames(string hiveIn, string keyIn, string pattern, ItemEntityVector* names, bool isRegex = true);
-	/** Get keys foir the specifed regex.
-		Attempt to locate any constant portion of the registry key. If a constant
-		portion can be found then start the pattern matching search from the end of the 
-		constant portion to save time. This cuts the time to 1/20th of the old time, nad
-		reduces the memory usage of this probe to 1/30th of the old memory usage.
-	*/
-	void KeyPatternMatch(ItemEntity* hive, string pattern, ItemEntityVector* keys, bool isRegex = true);
-	/** Convert the registry data to string representations and add to the provided Item. */
-	void RetrieveInfo(string, string, string, DWORD, LPBYTE, DWORD, Item*);
+	/** Return a new Item containing the data for the specified registry item.
+	 *  @param hive A string that contains the hive of the registry key that you want to obtain.
+	 *  @param key A string that contains the key of the registry key that you want to obtain.
+     *  @param name A string that contains the name of the registry key value that you want to obtain.
+     *  @return The item that represents the registry key that uses the specified hive, key, and name.
+     */
+	Item* GetRegistryKey(string hive, string key, string name);
 
-	/** Return true if the specified key exists under the specified hive. */
-	bool KeyExists(string hive, string key);
-	/** Return true if the specified name exists under the specified hive and key. */
-	bool NameExists(string hive, string key, string name);
+	/** Convert the registry data to string representations and add to the provided Item.
+	 *  @param hiveIn A string that contains the hive of the registry key that you want to obtain information for.
+	 *  @param keyIn A string that contains the key of the registry key that you want to obtain information for.
+     *  @param nameIn A string that contains the name of the registry key value that you want to obtain information for.
+	 *  @param typeIn A DWORD value that specifies the type of the registry key value.
+	 *  @param valueIn A LBYTE value that contains the value of the registry key value.
+	 *  @param valuelenIn DWORD value that contains the length of the registry key value.
+	 *  @param item A item that is used to store the registry data.
+     *  @return Void.
+     */
+	void RetrieveInfo(string hiveIn, string keyIn, string nameIn, DWORD typeIn, LPBYTE valueIn, DWORD valuelenIn, Item* item);
+
+	/** The static instance of the RegistryProbe.
+	*  All Probes are singletons. The ProbeFactory is responsible for managing instances of Probes.
+	*/
+	static RegistryProbe* instance;
 };
 
 #endif
