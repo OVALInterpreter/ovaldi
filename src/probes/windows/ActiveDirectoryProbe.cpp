3c34edfa3a747e841039fb99f61087e36d8ebdf6
Dan Haynes <dhaynes@mitre.org>
2010-01-04 20:43:44 +0000

    Updated the copyright dates from 2002-2009 to 2002-2010.

--- src/probes/windows/ActiveDirectoryProbe.cpp
+++ src/probes/windows/ActiveDirectoryProbe.cpp
@@ -1,6 +1,6 @@
 //
 //****************************************************************************************//
-// Copyright (c) 2002-2009, The MITRE Corporation
+// Copyright (c) 2002-2010, The MITRE Corporation
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are
