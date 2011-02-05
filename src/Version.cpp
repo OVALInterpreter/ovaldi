59fdf0c59dbf2f24fbb181d9b7ef0506f5b8b91a
Michael Chisholm <chisholm@mitre.org>
2011-02-05 18:47:52 +0000

    Updating version info for schema & interpreter version 5.9

--- src/Version.cpp
+++ src/Version.cpp
@@ -39,10 +39,10 @@ using namespace std;
 #define MAJOR_VERSION "5"
 #define MIN_COMPATIBLE_VERSION "5.0"
 #define VENDOR "The MITRE Corporation"
-#define VERSION "5.8"
-#define BUILD 2
+#define VERSION "5.9"
+#define BUILD 1
 
-#define SCHEMA_VERSION "5.8"
+#define SCHEMA_VERSION "5.9"
 
 string Version::GetMinumumCompatibleVersion() {
 
