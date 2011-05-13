e69a5bc8692ab0daafee141ec28d05b3928fc37a
Michael Chisholm <chisholm@mitre.org>
2011-05-13 23:49:34 +0000

    Modified to use the NetworkInterfaces module to get info about network interfaces.

--- src/linux/SystemInfo.cpp
+++ src/linux/SystemInfo.cpp
@@ -57,15 +57,6 @@ SystemInfo::~SystemInfo() {
 	//	Delete all objects in the interfaces vector.
 	//
 	// -----------------------------------------------------------------------
-
-	IfData *tmp	= NULL;
-	while(interfaces.size() !=0) {
-
-		tmp = interfaces[interfaces.size()-1];
-		interfaces.pop_back();
-		delete tmp;
-		tmp = NULL;
-	}
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
@@ -101,25 +92,22 @@ void SystemInfo::Write(XERCES_CPP_NAMESPACE_QUALIFIER DOMDocument *scDoc)
 	sysInfoNode->appendChild(interfacesElm);
 
 	// Loop through contents of the interfaces vector and write each IfData objet
-	IfDataVector::iterator iterator;
+	IntfList::iterator iterator;
 	for (iterator=interfaces.begin(); iterator!=interfaces.end(); iterator++) {
 
-		if((*iterator)->ipAddress.compare("127.0.0.1") != 0) {
+		// Create a new interface element
+		DOMElement* interfaceElm = XmlCommon::CreateElement(scDoc, "interface");
+		interfacesElm->appendChild(interfaceElm);
 
-			// Create a new interface element
-			DOMElement* interfaceElm = XmlCommon::CreateElement(scDoc, "interface");
-			interfacesElm->appendChild(interfaceElm);
-
-			// Add the childer to the inerface element
-			tmpElm = XmlCommon::CreateElement(scDoc, "interface_name", (*iterator)->ifName);
-			interfaceElm->appendChild(tmpElm);
+		// Add the childer to the inerface element
+		tmpElm = XmlCommon::CreateElement(scDoc, "interface_name", iterator->GetName());
+		interfaceElm->appendChild(tmpElm);
 			
-			tmpElm = XmlCommon::CreateElement(scDoc, "ip_address", (*iterator)->ipAddress);
-			interfaceElm->appendChild(tmpElm);
+		tmpElm = XmlCommon::CreateElement(scDoc, "ip_address", inet_ntoa(iterator->GetIPAddr()));
+		interfaceElm->appendChild(tmpElm);
 
-			tmpElm = XmlCommon::CreateElement(scDoc, "mac_address", (*iterator)->macAddress);
-			interfaceElm->appendChild(tmpElm);
-		}
+		tmpElm = XmlCommon::CreateElement(scDoc, "mac_address", iterator->GetHwAddr());
+		interfaceElm->appendChild(tmpElm);
 	}
 }
 
@@ -204,7 +192,7 @@ void SystemInfoCollector::GetOSInfo(SystemInfo *sysInfo) {
 	free(chHostName);
 }
 
-IfDataVector SystemInfoCollector::GetInterfaces() {
+IntfList SystemInfoCollector::GetInterfaces() {
 	//------------------------------------------------------------------------------------//
 	//
 	//  ABSTRACT
@@ -215,120 +203,19 @@ IfDataVector SystemInfoCollector::GetInterfaces() {
 	//	Must get interface_name, ip_address, and mac_address for each interface
 	//------------------------------------------------------------------------------------//
 
-	IfDataVector interfaces;
-	
-	unsigned char      *u;
-	int                sockfd, size  = 1;
-	struct ifreq       *ifr;
-	struct ifconf      ifc;
-	struct sockaddr_in sa;
-
-	if (0 > (sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP))) {
-	  throw SystemInfoException("Error: Unable to open socket.");
-	}	
-
-	ifc.ifc_len = IFRSIZE;
-	ifc.ifc_req = NULL;
-
-	do {
-	++size;
-	/* realloc buffer size until no overflow occurs  */
-
-		if (NULL == (ifc.ifc_req = (ifreq*)realloc(ifc.ifc_req, IFRSIZE))) {
-		  throw SystemInfoException("Error: Unable to allocate mememory.");
-		}
-		ifc.ifc_len = IFRSIZE;
-		if (ioctl(sockfd, SIOCGIFCONF, &ifc)) {
-		  throw SystemInfoException("Error: ioctl SIOCFIFCONF.");
-		}
-	} while  (IFRSIZE <= ifc.ifc_len);
-
-	ifr = ifc.ifc_req;
-
-	for (;(char *) ifr < (char *) ifc.ifc_req + ifc.ifc_len; ++ifr) {
-
-		if (ifr->ifr_addr.sa_data == (ifr+1)->ifr_addr.sa_data) {
-		  continue;  // duplicate, skip it
-		}
-
-		if (ioctl(sockfd, SIOCGIFFLAGS, ifr)) {
-		  continue;  // failed to get flags, skip it
-		}
-
-		//printf("Interface:  %s\n", ifr->ifr_name);
-		//printf("IP Address: %s\n", inet_ntoa(inaddrr(ifr_addr.sa_data)));
-		IfData *tmpIfData = new IfData();
-		tmpIfData->ifName = ifr->ifr_name;
-		tmpIfData->ipAddress = inet_ntoa(inaddrr(ifr_addr.sa_data));
-
-		if (0 == ioctl(sockfd, SIOCGIFHWADDR, ifr)) {
-
-			/* Select which  hardware types to process.
-			*
-			*    See list in system include file included from
-			*    /usr/include/net/if_arp.h  (For example, on
-			*    Linux see file /usr/include/linux/if_arp.h to
-			*    get the list.)
-			*/
-		  /* No used 
-			switch (ifr->ifr_hwaddr.sa_family) {
-
-				default:
-					printf("\n");
-					continue;
-				case  ARPHRD_NETROM:  case  ARPHRD_ETHER:  case  ARPHRD_PPP:
-				case  ARPHRD_EETHER:  case  ARPHRD_IEEE802: break;
-			}
-		  */
-
-		  
-			u = (unsigned char *) &ifr->ifr_addr.sa_data;
-			char *macStr = (char*)malloc(sizeof(char)*128);
-			memset(macStr, 0, 128);
-			if (u[0] + u[1] + u[2] + u[3] + u[4] + u[5]) {
-			  //printf("HW Address: %2.2x.%2.2x.%2.2x.%2.2x.%2.2x.%2.2x\n", u[0], u[1], u[2], u[3], u[4], u[5]);
-			  sprintf(macStr, "%2.2X-%2.2X-%2.2X-%2.2X-%2.2X-%2.2X", u[0], u[1], u[2], u[3], u[4], u[5]); 
-			  tmpIfData->macAddress = macStr;
-			}
-			free(macStr);
-
-			interfaces.push_back(tmpIfData);
-		}
-
-		/* netmask 
-		if (0 == ioctl(sockfd, SIOCGIFNETMASK, ifr) && strcmp("255.255.255.255", inet_ntoa(inaddrr(ifr_addr.sa_data)))) {
-
-			printf("Netmask:    %s\n", inet_ntoa(inaddrr(ifr_addr.sa_data)));
-		}
-		*/
-
-		/* broadcast
-		if (ifr->ifr_flags & IFF_BROADCAST) {
-
-			if (0 == ioctl(sockfd, SIOCGIFBRDADDR, ifr) && strcmp("0.0.0.0", inet_ntoa(inaddrr(ifr_addr.sa_data)))) {
-				printf("Broadcast:  %s\n", inet_ntoa(inaddrr(ifr_addr.sa_data)));
-			}
-		}
-		*/
-		
-		/* MTU
-		if (0 == ioctl(sockfd, SIOCGIFMTU, ifr)) {
-
-			printf("MTU:        %u\n",  ifr->ifr_mtu);
-		}
-		*/
-
-		/* Metric
-		if (0 == ioctl(sockfd, SIOCGIFMETRIC, ifr)) {
-			printf("Metric:     %u\n",  ifr->ifr_metric);
-		}
-		printf("\n");
-		*/
+	using namespace NetworkInterfaces;
+	list<Interface> intfs = GetAllInterfaces();
+	for (list<Interface>::iterator iter = intfs.begin();
+		 iter != intfs.end();
+		 ) {
+		// only ethernet interfaces are supported for now, filter out others.
+		if (iter->GetType() == Interface::ETHERNET)
+			++iter;
+		else
+			iter = intfs.erase(iter);
 	}
 
-	close(sockfd);
-
-	return interfaces;
+	return intfs;
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
